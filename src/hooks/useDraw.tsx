0f0321c540c8c8d04b79acaa2b6d06fe0d458212
r3nanp <renanmol87@gmail.com>
2021-05-27 10:28:08 -0300

    feat: create useDraw hook

---
+++ src/hooks/useDraw.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { DrawContext } from '../contexts/DrawContext'
+
+export const useDraw = () => {
+  const context = React.useContext(DrawContext)
+  
+  return context
+}
