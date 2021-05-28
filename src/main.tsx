79deeec01f92099256f06d1b7d99416bf4de1f90
r3nanp <renanmol87@gmail.com>
2021-05-28 09:43:10 -0300

    feat: add theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
 import App from './App'
+
 import { DrawProvider } from './contexts/DrawContext'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles/theme'
+import { GlobalStyles } from './styles/globals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <DrawProvider>
-      <App />
-    </DrawProvider>
+    <ThemeProvider theme={theme}>
+      <DrawProvider>
+        <GlobalStyles />
+        <App />
+      </DrawProvider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
