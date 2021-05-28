d5bb6e5c47dde74983de4fc0ca17aae8e61abb44
r3nanp <renanmol87@gmail.com>
2021-05-28 09:42:53 -0300

    feat: add theme config

---
+++ src/styles/globals.ts
@@ -0,0 +1,30 @@
+import {
+  createGlobalStyle,
+  DefaultTheme,
+  css,
+  GlobalStyleComponent
+} from 'styled-components'
+
+type GlobalStylesProps = {
+  removeBg?: boolean
+}
+
+export const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body, html {
+    ${({ theme }) => css`
+      background: ${theme.colors.primary};
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      font-family: ${theme.fonts.family};
+    `}
+  }
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    primary: '#f1f2f5'
+  },
+  fonts: {
+    family:
+      '-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;'
+  }
+}
