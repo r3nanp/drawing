c00244fb67468054f7cba8d05f25f90320a9b9c6
r3nanp <renanmol87@gmail.com>
2021-05-28 22:54:23 -0300

    feat: add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
 import { Draw } from './components/Draw'
+import { PaintingDashboard } from './components/PaintingDashboard'
 
 export default function App() {
   return (
     <>
+      <PaintingDashboard />
       <Draw />
     </>
   )
