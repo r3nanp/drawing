71d34ea9afdb1b2892079012271cc501302cd802
r3nanp <renanmol87@gmail.com>
2021-05-28 22:46:15 -0300

    refactor: remove effect

--- src/components/Draw/index.tsx
+++ src/components/Draw/index.tsx
@@ -1,15 +1,8 @@
 import * as React from 'react'
 import { useDraw } from '../../hooks/useDraw'
 
-import styles from './styles.module.css'
-
 export const Draw = () => {
-  const { startDrawing, stopDrawing, draw, canvasRef, prepareCanvas } =
-    useDraw()
-
-  React.useEffect(() => {
-    prepareCanvas()
-  }, [])
+  const { startDrawing, stopDrawing, draw, canvasRef } = useDraw()
 
   return (
     <canvas
@@ -17,7 +10,6 @@ export const Draw = () => {
       onMouseDown={startDrawing}
       onMouseUp={stopDrawing}
       onMouseMove={draw}
-      className={styles.canvas}
     />
   )
 }
