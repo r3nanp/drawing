fee9d9a91599cf934fa49c58ae2b1d8d409537d8
r3nanp <renanmol87@gmail.com>
2021-06-14 13:56:39 -0300

    feat: fix responsive errors

--- src/pages/Drawing/index.tsx
+++ src/pages/Drawing/index.tsx
@@ -2,11 +2,28 @@ import * as React from 'react'
 import { Draw } from '../../components/Draw'
 import { PaintingDashboard } from '../../components/PaintingDashboard'
 
+import * as S from './styles'
+
 export default function Drawing() {
   return (
     <>
-      <Draw />
-      <PaintingDashboard />
+      <S.Container>
+        <Draw />
+        <PaintingDashboard />
+      </S.Container>
+
+      <S.Warning>
+        <div>
+          <h3>This site is optimized for desktop</h3>
+          <p>
+            Please visit &nbsp;
+            <a href="https://drawing.r3nanp.vercel.app">
+              drawing.r3nanp.vercel.app
+            </a>
+            &nbsp; on a desktop to start drawing!
+          </p>
+        </div>
+      </S.Warning>
     </>
   )
 }
--- src/pages/Drawing/styles.ts
+++ src/pages/Drawing/styles.ts
@@ -1,5 +1,55 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components'
 
-export const Container = styled.div`
+export const Container = styled.section`
+  display: flex;
 
-`;
+  @media (max-width: 768px) {
+    display: none;
+  }
+`
+
+export const Warning = styled.div`
+  ${({ theme }) => css`
+    background: ${theme.colors.primary};
+
+    width: 100vw;
+    height: 100vh;
+
+    display: none;
+    padding: 1rem;
+
+    div {
+      padding: 1.25rem 1.5rem;
+
+      background: ${theme.colors.white};
+      box-shadow: ${theme.shadow.magical};
+      border-radius: 5px;
+
+      h3 {
+        text-align: center;
+        font-size: 1.5rem;
+        line-height: 1.5rem;
+
+        margin-bottom: 0.5rem;
+      }
+
+      p {
+        font-size: 1.2rem;
+        line-height: 1.5rem;
+        padding-top: 0.5rem;
+        text-align: center;
+
+        color: ${theme.colors.text};
+
+        a {
+          color: inherit;
+        }
+      }
+    }
+
+    @media (max-width: 768px) {
+      display: grid;
+      place-items: center;
+    }
+  `}
+`
